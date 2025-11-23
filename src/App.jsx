960ecf175dc7859fdf2eaa97188857c26ec313ce
kavinda1996 <gayashankavinda95@gmail.com>
2025-11-23 13:07:43 +0530

    load ui using bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,10 @@ import './App.css'
 import Product from './components/Product'
 
 function App() {
- 
-
+  
   return (
     <>
-      
+      <Product />
     </>
   )
 }
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,21 +1,77 @@
-import React from 'react'
+import React from "react";
 
 function Product() {
-    return (
-        <div>
+  return (
+    <div className="container py-5">
+      <div className="row py-lg-5">
+        <div className="col-lg-6 col-md-8 mx-auto text-center">
+          <h1 className="fw-light">Album example</h1>
+          <p className="lead text-body-secondary">
+            Something short and leading about the collection below—its contents,
+            the creator, etc. Make it short and sweet.
+          </p>
 
-            <div class="row py-lg-5">
-                <div class="col-lg-6 col-md-8 mx-auto">
-                    <h1 class="fw-light">Album example</h1>
-                    <p class="lead text-body-secondary">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
-                    <p> <a href="#" class="btn btn-primary my-2">Main call to action</a>
-                        <a href="#" class="btn btn-secondary my-2">Secondary action</a></p>
-                </div>
-            </div>
-
-
+          <p>
+            <a href="#" className="btn btn-primary my-2">
+              Main call to action
+            </a>
+            <a href="#" className="btn btn-secondary my-2 ms-2">
+              Secondary action
+            </a>
+          </p>
         </div>
-    )
+      </div>
+
+      {/* CARD */}
+      <div className="row">
+        <div className="col-md-4">
+          <div className="card shadow-sm">
+            <svg
+              aria-label="Placeholder: Thumbnail"
+              className="bd-placeholder-img card-img-top"
+              width="100%"
+              height="225"
+              xmlns="http://www.w3.org/2000/svg"
+              role="img"
+              preserveAspectRatio="xMidYMid slice"
+              focusable="false"
+            >
+              <title>Placeholder</title>
+              <rect width="100%" height="100%" fill="#55595c"></rect>
+              <text x="50%" y="50%" fill="#eceeef" dy=".3em">
+                Thumbnail
+              </text>
+            </svg>
+
+            <div className="card-body">
+              <p className="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content.
+              </p>
+
+              <div className="d-flex justify-content-between align-items-center">
+                <div className="btn-group">
+                  <button
+                    type="button"
+                    className="btn btn-sm btn-outline-secondary"
+                  >
+                    View
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-sm btn-outline-secondary"
+                  >
+                    Edit
+                  </button>
+                </div>
+                <small className="text-body-secondary">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Product
+export default Product;
